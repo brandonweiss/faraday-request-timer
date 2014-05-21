1ef308e11abbbaacee5a357c93dbfecbe6d21cb2
Brandon Weiss <brandon@anti-pattern.com>
2014-05-21 09:06:53 -0700

    Fixing indentation

--- lib/faraday/request/timer.rb
+++ lib/faraday/request/timer.rb
@@ -3,13 +3,13 @@ module Faraday
 
     Faraday::Request.register_middleware :timer => self
 
-      def call(env)
-        started_at = Time.now
-        app        = @app.call(env)
-        ended_at   = Time.now
+    def call(env)
+      started_at = Time.now
+      app        = @app.call(env)
+      ended_at   = Time.now
 
-        app.env[:duration] = ended_at - started_at
-        app
+      app.env[:duration] = ended_at - started_at
+      app
     end
 
   end
